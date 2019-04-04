716f9304872b25f0e27debccddb4cba1ddcfb831
yeajunseok <yeajunseok@gmail.com>
2019-04-04 14:55:15 +0900

    Beautifulsoup 기초(2)까지 정리 파일 작성 완료

---
+++ 정리_find_all을 이용하여 태그 사용법과 정규표현식 사용법.py
@@ -0,0 +1,86 @@
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+#위에 4줄 한이유는 Atom에서 한글을 출력하지 못하기 떄문에 문제 해결하기 위해 설정한다.
+
+html = """
+<html><body>
+  <ul>
+    <li><a href="http://www.naver.com">naver</a></li>
+    <li><a href="http://www.daum.net">daum</a></li>
+    <li><a href="https://www.google.com">google</a></li>
+    <li><a href="https://www.tistory.com">tistory</a></li>
+  </ul>
+</body></html>
+"""
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser')
+
+##find_all 사용법##
+links_1 = soup.find_all("a") #a태그를 link변수에 한방에 담는다.
+print(links_1)
+    #결과 > [<a href="http://www.naver.com">naver</a>, <a href="http://www.daum.net">daum</a>, <a href="https://www.google.com">google</a>, <a href="https://www.tistory.com">tistory</a>]
+links_2 = soup.find_all("a", string="daum")
+print(links_2)
+    #결과 > [<a href="http://www.daum.net">daum</a>]
+links_3 = soup.find_all("a", limit=3) #3개만 갖고온다.
+print(links_3)
+    #결과 > [<a href="http://www.naver.com">naver</a>, <a href="http://www.daum.net">daum</a>, <a href="https://www.google.com">google</a>]
+links_4 = soup.find_all(string=["naver","google"])
+print(links_4)
+    #결과 > ['naver', 'google']
+for a in links_1:
+    print(a)
+    print(a.string)         #naver
+    print(a.attrs['href'])  #http://www.naver.com 등등등...
+#결과: <a href="http://www.naver.com">naver</a>
+#      naver
+#      http://www.naver.com
+
+#결과: <a href="http://www.daum.net">daum</a>
+#      daum
+#      http://www.daum.net
+
+#결과: <a href="https://www.google.com">google</a>
+#      google
+#      https://www.google.com
+
+#결과: <a href="https://www.tistory.com">tistory</a>
+#      tistory
+#      https://www.tistory.com
+'''
+print(type(links_1))
+#결과: <class 'bs4.element.ResultSet'>
+#알수 있는점: find_all의 타입은 ResultSet이다!!
+'''
+
+##find_all에서 정규표현식 사용법##
+import re
+li_1 = soup.find_all(href=re.compile(r"^https://"))
+print(li_1)
+    #결과값: [<a href="https://www.google.com">google</a>, <a href="https://www.tistory.com">tistory</a>]
+for e in li_1:
+    print(e.attrs['href'])
+    #결과값: https://www.google.com
+    #결과값: https://www.tistory.com
+
+li_2 = soup.find_all(href=re.compile(r"da"))
+for e in li_2:
+    print(e.ettrs['href'])
+    #결과값: http://www.daum.net
+    #결과값: http://www.daum.com
+'''
+soup.find_all(href=re.compile(r"^https://"))와
+soup.find_all("a") 의 차이점
+
+links_1 = soup.find_all("a") #a태그를 변수에 한방에 전부 담는다.
+print(links_1)
+    #결과값: [<a href="http://www.naver.com">naver</a>, <a href="http://www.daum.net">daum</a>, <a href="https://www.google.com">google</a>, <a href="https://www.tistory.com">tistory</a>]
+
+li = soup.find_all(href=re.compile(r"^https://")) #정규표현식에 맞는 값만 담는다.
+print(li)
+    #결과값: [<a href="https://www.google.com">google</a>, <a href="https://www.tistory.com">tistory</a>]
+
+!!!그러나 CSS선택자를 거의 많이 사용한다. 그냥 여기선 정규표현식으로도 뽑을수 있구나 정도만 알고 가자
+'''
---
+++ 정리_select_all을 이용하여 CSS선택자와 정규표현식 사용법.py
@@ -0,0 +1,64 @@
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+#위에 4줄 한이유는 Atom에서 한글을 출력하지 못하기 떄문에 문제 해결하기 위해 설정한다.
+
+html = """
+<html><body>
+<div id="main">
+  <h1>강의목록</h1>
+  <ul class="lecs">
+    <li>Java 초고수 되기</li>
+    <li>파이썬 기초 프로그래밍</li>
+    <li>파이썬 머신러닝 프로그래밍</li>
+    <li>안드로이드 블루투스 프로그래밍</li>
+  </ul>
+</div>
+</body></html>
+"""
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser')
+
+######CSS선택자를 이용한######
+##select() 사용법##
+se1 = soup.select("div#main > h1")
+print(type(se1))     #결과: <class 'list'>
+print(se1)           #결과: [<h1>강의목록</h1>]
+print(se1[0].string) #결과: 강의목록
+for z in se1:
+    print(z.string)
+                    #결과: 강의목록
+
+se1_1 = soup.select("div#main > ul.lecs > li")
+print(se1_1)        #결과: [<li>Java 초고수 되기</li>, <li>파이썬 기초 프로그래밍</li>, <li>파이썬 머신러닝 프로그래밍</li>, <li>안드로이드 블루투스 프로그래밍</li>]
+for s in se1_1:
+    print(s.string) #결과: Java 초고수 되기 / 파이썬 기초 프로그래밍 / 파이썬 머신러닝 프로그래밍 / 안드로이드 블루투스 프로그래밍
+
+##select_one() 사용법##
+se2 = soup.select_one("div#main > h1")
+print(type(se2))    #결과: <class 'bs4.element.Tag'>
+print(se2)          #결과: <h1>강의목록</h1>
+print(se2.string)   #결과: 강의목록
+
+##select와 select_one의 차이점##
+##가지고 올때 딱 하나면 select_one을 사용하고, 그게 아니면 select사용한다.
+
+######정규식을 이용한######
+##select_one과 select##
+print(soup.select_one("li:nth-of-type(1)").string)              #결과: 닭도리탕
+print(soup.select_one("#ac-list > li:nth-of-type(4)").string)   #결과: 양주
+print(soup.select("#ac-list > li[data-lo='cn']")[0].string)     #결과: 양주, select는 list형태로 반환한다.
+print(soup.select("#ac-list > li.alcohol.high")[0].string)      #결과: 양주
+print(soup.select("#ac-list > li[class='alcohol']")[1].string)  #결과: 맥주, select는 list형태로 반환한다.
+
+##find와 find_all##
+param = {"data-lo":"cn", "class":"alcohol"}
+print(soup.find("li",param).string)                             #결과: 양주
+print(soup.find(id="ac-list").find("li",param).string)          #결과:양주
+
+for ac in soup.find_all("li"):
+    if ac['data-lo'] == 'us':
+        print('data-lo == us', ac.string)
+                                                                #결과: data-lo == us 스테이크
+                                                                #     data-lo == us 맥주
